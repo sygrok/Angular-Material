68f2e542fed3eb7d4add6b37b7f47658674c6384
Berkay Anduv <sygrok@hotmail.com>
2023-06-29 21:27:41 +0300

    update

---
+++ angularMaterial/src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports:[MatButtonModule,MatIconModule]
+})
+export class MaterialModule { }
